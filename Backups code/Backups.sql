8a1bdc21268d0a97a2a1bbbbd1563ed14279fccc
Anthony <anthonymoro08@gmail.com>
2025-01-07 22:39:50 -0800

    [update] backups code
    Se actualiza el codigo para que el los backups seguarden con la fecha en el nombre

--- Backups code/Backups.sql
+++ Backups code/Backups.sql
@@ -1,40 +1,78 @@
 -- RESPALDO FULL
-BACKUP DATABASE [DB_InventariosGlobal] TO DISK = N'C:\DB\DB_InventariosGlobal.bak'
-WITH NOFORMAT, NOINIT, NAME = N'Prueba-Full Database Backup',
-STATS = 10
+DECLARE @FechaHora NVARCHAR(20)
+SET @FechaHora = FORMAT(GETDATE(), 'yyyyMMdd_HHmmss')
+
+DECLARE @BackupPath NVARCHAR(255)
+SET @BackupPath = N'C:\DB\DB_InventariosGlobal_' + @FechaHora + '.bak'
+
+BACKUP DATABASE [DB_InventariosGlobal] 
+TO DISK = @BackupPath
+WITH NOFORMAT, NOINIT, 
+     NAME = N'Prueba-Full Database Backup',
+     STATS = 10;
 GO
 
 -- RESTAURAR RESPALDO
-RESTORE FILELISTONLY FROM DISK = 'C:\DB\DB_InventariosGlobal.bak'
-RESTORE DATABASE DB_InventariosGlobal FROM DISK = 'C:\DB\DB_InventariosGlobal.bak'
+ALTER DATABASE DB_InventariosGlobal SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+RESTORE DATABASE DB_InventariosGlobal 
+FROM DISK = 'C:\DB\DB_InventariosGlobal_.bak' -- Cambiar por el nombre del archivo generado
 WITH MOVE 'DB_InventariosGlobal' TO 'C:\DB\DB_InventariosGlobal.mdf',
-			MOVE 'DB_InventariosGlobal_log' TO 'C:\DB\DB_InventariosGlobal_log.ldf',
-			REPLACE, STATS = 10, recovery
+     MOVE 'DB_InventariosGlobal_log' TO 'C:\DB\DB_InventariosGlobal_log.ldf',
+     REPLACE, 
+     STATS = 10, 
+     RECOVERY;
+GO
+
+ALTER DATABASE DB_InventariosGlobal SET MULTI_USER;
+GO
+
 
 ------------------------------------------------------------------------------------
 
 -- RESPALDO DIFERENCIAL
-BACKUP DATABASE [DB_InventariosGlobal] TO DISK = N'C:\DB\DB_InventariosGlobal.dif'
-WITH DIFFERENTIAL, NOFORMAT, NOINIT, NAME = N'Prueba-Dif Database Backup',
-SKIP, NOREWIND, NOUNLOAD, STATS = 10
+DECLARE @BackupFileName NVARCHAR(MAX);
+SET @BackupFileName = 'C:\DB\DB_InventariosGlobal_' 
+                      + FORMAT(GETDATE(), 'yyyyMMdd_HHmmss') + '.dif';
+
+BACKUP DATABASE [DB_InventariosGlobal] 
+TO DISK = @BackupFileName
+WITH DIFFERENTIAL, NOFORMAT, NOINIT, 
+NAME = N'Prueba-Dif Database Backup',
+SKIP, NOREWIND, NOUNLOAD, STATS = 10;
 GO
 
 -- RESTAURAR RESPALDO
-RESTORE DATABASE DB_InventariosGlobal FROM DISK = 'C:\DB\DB_InventariosGlobal.dif'
+RESTORE DATABASE DB_InventariosGlobal 
+FROM DISK = 'C:\DB\DB_InventariosGlobal_.dif' -- Cambiar por el nombre del archivo generado
 WITH MOVE 'DB_InventariosGlobal' TO 'C:\DB\DB_InventariosGlobal.mdf',
-			MOVE 'DB_InventariosGlobal_log' TO 'C:\DB\DB_InventariosGlobal_log.ldf',
-			REPLACE, STATS = 10, recovery
+     MOVE 'DB_InventariosGlobal_log' TO 'C:\DB\DB_InventariosGlobal_log.ldf',
+     REPLACE, 
+     STATS = 10, 
+     RECOVERY;
+GO
 
 ------------------------------------------------------------------------------------
 
 -- RESPALDO TRANSACIONAL
-BACKUP LOG [DB_InventariosGlobal] TO DISK = N'C:\DB\DB_InventariosGlobal.trn'
-WITH NOFORMAT, NOINIT, NAME = N'Prueba-Trn Database Backup',
-SKIP, NOREWIND, NOUNLOAD, STATS = 10
+DECLARE @BackupFileName NVARCHAR(MAX);
+SET @BackupFileName = 'C:\DB\DB_InventariosGlobal_' 
+                      + FORMAT(GETDATE(), 'yyyyMMdd_HHmmss') + '.trn';
+
+BACKUP LOG [DB_InventariosGlobal] 
+TO DISK = @BackupFileName
+WITH NOFORMAT, NOINIT, 
+NAME = N'Prueba-Trn Database Backup',
+SKIP, NOREWIND, NOUNLOAD, STATS = 10;
 GO
 
 -- RESTAURAR RESPALDO
-RESTORE DATABASE DB_InventariosGlobal FROM DISK = 'C:\DB\DB_InventariosGlobal2.trn'
+RESTORE DATABASE DB_InventariosGlobal 
+FROM DISK = 'C:\DB\DB_InventariosGlobal_.trn' -- Cambiar por el nombre del archivo generado
 WITH MOVE 'DB_InventariosGlobal' TO 'C:\DB\DB_InventariosGlobal.mdf',
-			MOVE 'DB_InventariosGlobal_log' TO 'C:\DB\DB_InventariosGlobal_log.ldf',
-			REPLACE, STATS = 10, recovery, STOPAT = '20250107 12:30:00.000'
+     MOVE 'DB_InventariosGlobal_log' TO 'C:\DB\DB_InventariosGlobal_log.ldf',
+     REPLACE, 
+     STATS = 10, 
+     RECOVERY;
+GO
